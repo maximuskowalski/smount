4cc9d6bd4253b02991f8d6533c5e376b70f60fe6
MaxKowalski <matthewbarger@gmail.com>
2019-10-25 08:44:06 +1100

    new var method, remove test file

--- exchanger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export user="max" group="max"
-envsubst '$user,$group' <./input/teamdrive@.service >./output/teamdrive@.service
-envsubst '$user,$group' <./input/teamdrive_primer@.service >./output/teamdrive_primer@.service
-envsubst '$user,$group' <./input/teamdrive_primer@.timer >./output/teamdrive_primer@.timer
--- mountup.sh
+++ mountup.sh
@@ -21,7 +21,7 @@ sudo systemctl enable teamdrive@.service
 sudo systemctl enable teamdrive_primer@.service
 sudo systemctl enable teamdrive_primer@.timer
 #
-# Note that count default starting number=5575
+# Note that port default starting number=5575
 # Read the current port no to be used then increment by +1
 get_port_no_count () {
   read count < port_no.count
@@ -30,9 +30,8 @@ get_port_no_count () {
 
 # config files
 make_config () {
-  for set_file in $@; do echo Set file is $set_file
-    column -t $SET_DIR/$set_file|sed '/^\s*#.*$/d'|\
-    while IFS=' ' read -r name other;do
+  sed '/^\s*#.*$/d' $SET_DIR/$1|\
+    while read -r name other;do
       get_port_no_count
       conf="
       RCLONE_RC_PORT=$count
@@ -40,43 +39,36 @@ make_config () {
       DESTINATION_DIR=/mnt/sharedrives/$name/
       ";
       echo "$conf" > /opt/sharedrives/$name.conf
-      echo /opt/sharedrives/$name.conf
     done
-  done
 }
 
 make_starter () {
-  for set_file in $@; do echo Set file is $set_file
-    column -t $SET_DIR/$set_file|sed '/^\s*#.*$/d'|\
-    while IFS=' ' read -r name other;do
+  sed '/^\s*#.*$/d' $SET_DIR/$1|\
+    while read -r name other;do
       echo "sudo systemctl enable teamdrive@$name.service && sudo systemctl enable teamdrive_primer@$name.service">>vfs_starter.sh
     done
-    column -t $SET_DIR/$set_file|sed '/^\s*#.*$/d'|\
-    while IFS=' ' read -r name other;do
+    sed '/^\s*#.*$/d' $SET_DIR/$1|\
+    while read -r name other;do
       echo "sudo systemctl start teamdrive@$name.service && sudo systemctl start teamdrive_primer@$name.service">>vfs_starter.sh
     done
-  done
 }
 
 make_vfskill () {
-  for set_file in $@; do echo Set file is $set_file
-    column -t $SET_DIR/$set_file|sed '/^\s*#.*$/d'|\
-    while IFS=' ' read -r name other;do
+  sed '/^\s*#.*$/d' $SET_DIR/$1|\
+    while read -r name other;do
       echo "sudo systemctl stop teamdrive@$name.service && sudo systemctl stop teamdrive_primer@$name.service">>vfs_kill.sh
     done
-    column -t $SET_DIR/$set_file|sed '/^\s*#.*$/d'|\
-    while IFS=' ' read -r name other;do
+    sed '/^\s*#.*$/d' $SET_DIR/$1|\
+    while read -r name other;do
       echo "sudo systemctl disable teamdrive@$name.service && sudo systemctl disable teamdrive_primer@$name.service">>vfs_kill.sh
     done
-  done
 }
 
-make_config $@
-make_starter $@
+make_config $1
+make_starter $1
 # daemon reload
 sudo systemctl daemon-reload
-make_vfskill $@
+make_vfskill $1
 chmod +x vfs_starter.sh
 ./vfs_starter.sh  #fire the starter
 echo "sharedrive vfs mounts complete"
-echo "now edit mergerfs service"
