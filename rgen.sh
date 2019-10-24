3acacfa3f454ae984579242fa33b3fbec6eb1b8d
MaxKowalski <barry@fartbox.party>
2019-10-24 21:30:38 +1100

    Fix quotes

--- rgen.sh
+++ rgen.sh
@@ -12,19 +12,21 @@ get_sa_count () {
 
 make_rc_sa_config () {
   sed '/^\s*#.*$/d' $SET_DIR/$1|\
-  while read -r name driveid;do echo'
+  while read -r name driveid;do 
+  get_sa_count
+  echo "
 [$name]
 type = drive
 scope = drive
 server_side_across_configs = true
 team_drive = $driveid
-service_account_file = "$sadir"/$count.json
-'>>rc_sa.config
+service_account_file = "$sadir/$count.json"
+">>rc_sa.config
   done; }
 
 make_client_id_config () {
   sed '/^\s*#.*$/d' $SET_DIR/$1|\
-  while read -r name driveid;do echo'
+  while read -r name driveid;do echo "
 [$name]
 type = drive
 scope = drive
@@ -33,7 +35,7 @@ team_drive = $driveid
 client_id = $client_id
 client_secret = $client_secret
 token = $token 
-'>>rc_cid.config
+">>rc_cid.config
   done; }
 
 make_rc_sa_config $1
