9a2d86888c970e634bbd4f131d8dd1f05e7fe098
Max Kowalski <13492750+maximuskowalski@users.noreply.github.com>
2020-07-18 09:56:31 +1000

    update
    update

--- varia.sh
+++ varia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-
 # VARIABLES
 USER=max
 GROUP=max
+INSPTH=/opt/smount
 SET_DIR="./sets/"
 SA_PATH=/opt/sa/mounts
 MOUNT_DIR=/mnt/sharedrives
-MSTYLE=aio
+MSTYLE=aio 
 BINARY=/usr/bin/rclone
 RW_MDIR='/mnt/local'
 RO_MDIR='/mnt/sharedrives/sd*'
@@ -26,7 +26,7 @@ check_firstrun () {
 }
 
 export_vars () {
-  export myuser=${USER} mygroup=${GROUP} mysapath=${SA_PATH} mystyle=${MSTYLE} mycname=${CNAME} mybinary=${BINARY} myrwmdir=${RW_MDIR} myromdir=${RO_MDIR} mymdir=${MDIR} mymergerservice=${MERGERSERVICE}
+  export myuser=${USER} mygroup=${GROUP} myinspth=${INSPTH} mysapath=${SA_PATH} mystyle=${MSTYLE} mycname=${CNAME} mybinary=${BINARY} myrwmdir=${RW_MDIR} myromdir=${RO_MDIR} myscndromdir=${SECNDRO_MDIR} mymdir=${MDIR} mymergerservice=${MERGERSERVICE}
 }
 
 get_port_no_count () {
@@ -40,10 +40,10 @@ get_sa_count () {
 }
 
 aio () {
-  envsubst '${myuser},${mygroup},${mysapath},${mybinary}' <./input/aio@.service >./output/aio@.service
-  envsubst '${myuser},${mygroup}' <./input/primer@.service >./output/aio.primer@.service
-  envsubst '${myuser},${mygroup},${MSTYLE}' <./input/primer@.timer >./output/aio.primer@.timer
-  envsubst '${myrwmdir},${myromdir},${mymdir}' <./input/smerger.service >./output/aio.merger.service
+  envsubst '${myuser},${mygroup},${mysapath},${mybinary},${myinspth}' <./input/aio@.service >./output/aio@.service
+  envsubst '${myuser},${mygroup},${mybinary},${mystyle},${myinspth}' <./input/primer@.service >./output/aio.primer@.service
+  envsubst '${myuser},${mygroup},${mystyle}' <./input/primer@.timer >./output/aio.primer@.timer
+  envsubst '${myrwmdir},${myromdir},${mymdir},${myscndromdir}' <./input/smerger.service >./output/aio.merger.service
   # place
   sudo bash -c 'cp ./output/aio@.service /etc/systemd/system/aio@.service'
   sudo bash -c 'cp ./output/aio.primer@.service /etc/systemd/system/aio.primer@.service'
@@ -103,52 +103,55 @@ service_account_file_path = ${SA_PATH}
 make_starter () {
   sed '/^\s*#.*$/d' $SET_DIR/$1|\
     while read -r name other;do
-      echo "sudo systemctl enable ${MSTYLE}@${name}.service && sudo systemctl enable ${MSTYLE}.primer@${name}.service">>${MSTYLE}.starter.sh
+      echo "sudo systemctl enable ${MSTYLE}@${name}.service && sudo systemctl enable ${MSTYLE}.primer@${name}.service">>./scripts/${MSTYLE}.starter.sh
     done
     sed '/^\s*#.*$/d' $SET_DIR/$1|\
     while read -r name other;do
-      echo "sudo systemctl start ${MSTYLE}@${name}.service">>${MSTYLE}.starter.sh
+      echo "sudo systemctl start ${MSTYLE}@${name}.service">>./scripts/${MSTYLE}.starter.sh
     done
 }
 
 make_restart () {
   sed '/^\s*#.*$/d' $SET_DIR/$1|\
     while read -r name other;do
-      echo "sudo systemctl restart ${MSTYLE}@${name}.service">>${MSTYLE}.restart.sh
+      echo "sudo systemctl restart ${MSTYLE}@${name}.service">>./scripts/${MSTYLE}.restart.sh
     done
 }
 
 make_primer () {
   sed '/^\s*#.*$/d' $SET_DIR/$1|\
     while read -r name other;do
-      echo "sudo systemctl start ${MSTYLE}.primer@${name}.service">>${MSTYLE}.primer.sh
+      echo "sudo systemctl start ${MSTYLE}.primer@${name}.service">>./scripts/${MSTYLE}.primer.sh
     done
 }
 
 make_vfskill () {
   sed '/^\s*#.*$/d' $SET_DIR/$1|\
     while read -r name other;do
-      echo "sudo systemctl stop ${MSTYLE}@${name}.service && sudo systemctl stop ${MSTYLE}.primer@${name}.service">>${MSTYLE}.kill.sh
+      echo "sudo systemctl stop ${MSTYLE}@${name}.service && sudo systemctl stop ${MSTYLE}.primer@${name}.service">>./scripts/${MSTYLE}.kill.sh
     done
     sed '/^\s*#.*$/d' $SET_DIR/"$1"|\
     while read -r name other;do
-      echo "sudo systemctl disable ${MSTYLE}@${name}.service && sudo systemctl disable ${MSTYLE}.primer@${name}.service">>${MSTYLE}.kill.sh
+      echo "sudo systemctl disable ${MSTYLE}@${name}.service && sudo systemctl disable ${MSTYLE}.primer@${name}.service">>./scripts/${MSTYLE}.kill.sh
     done
 }
 
 # Make Dirs
-sudo mkdir -p /opt/smount/sharedrives /opt/smount/backup /opt/smount/scripts /opt/smount/config /opt/smount/output
-sudo chown -R ${USER}:${GROUP} /opt/smount/sharedrives /opt/smount/backup /opt/smount/scripts /opt/smount/config /opt/smount/output
-sudo chmod -R 775 /opt/smount/sharedrives /opt/smount/backup /opt/smount/scripts /opt/smount/config /opt/smount/output
+mkdir ./{sharedrives,backup,scripts,config,output}
+sudo mkdir -p ./{sharedrives,backup,scripts,config,output}
+sudo chown -R ${USER}:${GROUP} ./{sharedrives,backup,scripts,config,output}
+sudo chmod -R 775 ./{sharedrives,backup,scripts,config,output}
 
 # rename existing starter and kill scripts if present can we make CNAME = MSTYLE for making scripts and moving?
 #   ( [ -e "sa.count" ] || echo ${CPORT} > "sa.count" )
 make_backups () {
-( [ -e "${MSTYLE}.starter.sh" ] && mv ${MSTYLE}.starter.sh ./backup/${MSTYLE}.starter`date +%Y%m%d%H%M%S`.sh > /dev/null 2>&1 )
-( [ -e "${MSTYLE}.primer.sh" ] && mv ${MSTYLE}.primer.sh ./backup/${MSTYLE}.primer`date +%Y%m%d%H%M%S`.sh )
-( [ -e "${MSTYLE}.kill.sh" ] && mv ${MSTYLE}.kill.sh ./backup/${MSTYLE}.kill`date +%Y%m%d%H%M%S`.sh )
-( [ -e "${MSTYLE}.restart.sh" ] && mv ${MSTYLE}.restart.sh ./backup/${MSTYLE}.restart`date +%Y%m%d%H%M%S`.sh )
+( [ -e "./scripts/${MSTYLE}.starter.sh" ] && mv "./scripts/${MSTYLE}.starter.sh" ./backup/${MSTYLE}.starter`date +%Y%m%d%H%M%S`.sh > /dev/null 2>&1 )
+( [ -e "./scripts/${MSTYLE}.primer.sh" ] && mv "./scripts/${MSTYLE}.primer.sh" ./backup/${MSTYLE}.primer`date +%Y%m%d%H%M%S`.sh )
+( [ -e "./scripts/${MSTYLE}.kill.sh" ] && mv "./scripts/${MSTYLE}.kill.sh" ./backup/${MSTYLE}.kill`date +%Y%m%d%H%M%S`.sh )
+( [ -e "./scripts/${MSTYLE}.restart.sh" ] && mv "./scripts/${MSTYLE}.restart.sh" ./backup/${MSTYLE}.restart`date +%Y%m%d%H%M%S`.sh )
+( [ -e "./config/smount.conf" ] && cp "./config/smount.conf" ./backup/smount`date +%Y%m%d%H%M%S`.conf )
 }
+
 # enable new services TEST LATER if we can pull it out of the mount functions and condense to this
 # sudo systemctl enable ${MSTYLE}@.service
 # sudo systemctl enable ${MSTYLE}.primer@.service
