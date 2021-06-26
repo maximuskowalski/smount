1b6971190cacd1d834dc446da16651bf043f9f76
Max Kowalski <matthewbarger@gmail.com>
2021-06-26 22:15:24 +1000

    Check for rclone beta and workaround
    add colour and spacing for clearer output

--- shmount.sh
+++ shmount.sh
@@ -4,68 +4,96 @@ IFS=$'\n\t'
 
 #________ VARS DO NOT EDIT
 
-SDNAME=                # a share drive rclone config entry
-SDID=                  # a share drive ID
-SAPATH=                # service account path "/opt/sa/mounts"
-DRVLIST=               # list of drives configured
-RCUNAME=               # name for rclone union config entry
-STRUNION=              # union upstream string (accumulates during drive additions)
-VFSCACHESIZE=          # maximum size for VFS cache
-STRMERGER=             # merger string
-MNTPNT=/mnt/${RCUNAME} #
-RCLONE_RC_PORT=5575    # initial port for VFS
-RCI=true               # install or update Rclone?
-RCB=false              # rclone beta?
-CBI=false              # cloudbox install?
-NOW=$(date)
+SDNAME=             # a share drive rclone config entry
+SDID=               # a share drive ID
+SAPATH=             # service account path "/opt/sa/mounts"
+DRVLIST=            # list of drives configured
+RCUNAME=            # name for rclone union config entry
+STRUNION=           # union upstream string (accumulates during drive additions)
+VFSCACHESIZE=       # maximum size for VFS cache
+STRMERGER=          # merger string
+RCLONE_RC_PORT=5575 # initial port for VFS
+RCI=true            # install or update Rclone?
+RCB=false           # rclone beta?
+CBI=false           # cloudbox install?
+
+MNTPNT=/mnt/${RCUNAME}
 FREESPACE=$(df -h --output=avail /home/"$USER")
 RECSPACE=$(("${FREESPACE//[^0-9]/}" * 80 / 100))
+VERSION=$(rclone --version 2>>errors | head -n 1)
+
+BOLD="$(tput bold)"        # bold
+RESET="$(tput sgr0)"       # reset
+RED="$(tput setaf 1)"      # red
+GREEN="$(tput setaf 2)"    # green
+YELLOW="$(tput setaf 3)"   # yellow
+BRED="$(tput setaf 9)"     # bright red
+BGREEN="$(tput setaf 10)"  # bright green
+BYELLOW="$(tput setaf 11)" # bright yellow
 
 #________ FUNCTIONS
 
 rooter() {
     if [ "$(whoami)" = root ]; then
-        echo "Running as root or with sudo is not supported. Exiting."
+        echo "${BRED} Running as root or with sudo is not supported. Exiting.${RESET}"
         exit
     fi
 }
 
 cloudboxer() {
-    read -r -p "Is this a cloudbox install?  [Y/N] : " i
+    read -r -p "${YELLOW}Is this a cloudbox install?  [Y/N] :${RESET}" i
     case $i in
     [yY])
-        echo -e "mergerfs content line will be displayed after smounting has occured"
+        echo -e "${YELLOW}mergerfs content line will be displayed after smounting has occured"
         CBI="true"
         # readmergerfs
         ;;
     [nN])
-        echo -e "thank you"
+        echo -e "${YELLOW}thank you"
         RCI="false"
         ;;
     esac
 }
 
+betachek() {
+    if
+        rclone --version 2>>errors | head -n 1 | grep -q 'beta'
+    then
+        echo you have "${VERSION}" installed
+        echo
+        echo "${YELLOW}this script will not work with rclone beta"
+        echo "select beta install to have smount"
+        echo "temporarily install rclone stable to complete"
+        echo "the config process and update to beta when done"
+        echo
+        rcloneinstall
+    else
+        rcloneinstall
+    fi
+}
+
 rcloneinstall() {
-    read -r -p "Would you like to install or update rclone?  [Y/N] : " i
+    read -r -p "${YELLOW}Would you like to install or update rclone?  [Y/N] :${RESET}" i
     case $i in
     [yY])
-        echo -e "rclone will be installed"
+        echo -e "${YELLOW}rclone will be installed"
         RCI="true"
         rclonebetaq
         ;;
     [nN])
-        echo -e "fine"
+        echo -e "${YELLOW}fine"
         RCI="false"
         ;;
     *)
-        echo "Invalid Option"
+        echo "${BRED}Invalid Option"
         rcloneinstall
         ;;
     esac
 }
 
 rclonebetaq() {
-    echo "Do you want rclone beta or stable?"
+    echo
+    echo "${YELLOW}Do you want rclone beta or stable?${RESET}"
     select yn in "Beta" "Stable" "No"; do
         case $yn in
         Beta)
@@ -87,46 +115,66 @@ rclonebetaq() {
 }
 
 rclonecheck() {
-    ([ $RCI = true ] && rclonebaker) || :
-    # fi
+    ([ $RCI = true ] && rclonesetup) || :
+}
+
+rclonecheckmate() {
+    ([ $RCB = true ] && rclonebaker) || :
+}
+
+rclonesetup() {
+    if [ $RCB = true ]; then
+        echo "${YELLOW}Installing rclone stable, rclone beta will be installed after config${GREEN}"
+        curl https://rclone.org/install.sh | sudo bash || :
+        clear
+        echo "${YELLOW}rclone stable installed for configuration bug"
+        echo "rclone beta will be installed after configuration is completed"
+        echo
+    else
+        echo "${GREEN}Installing rclone stable${GREEN}"
+        curl https://rclone.org/install.sh | sudo bash || :
+        VERSION=$(rclone --version 2>>errors | head -n 1)
+        clear
+        echo "${YELLOW}rclone stable installed"
+        echo "${VERSION}"
+        echo
+    fi
 }
 
 rclonebaker() {
-    if [ $RCB = true ]; then
-        echo "Installing rclone beta"
-        curl https://rclone.org/install.sh | sudo bash -s beta &>/dev/null || :
-        echo "rclone beta installed"
-    else
-        echo "Installing rclone stable"
-        curl https://rclone.org/install.sh | sudo bash &>/dev/null || :
-        echo "rclone stable installed"
-    fi
+    echo "${YELLOW}Now installing rclone beta${GREEN}"
+    curl https://rclone.org/install.sh | sudo bash -s beta || :
+    VERSION=$(rclone --version 2>>errors | head -n 1)
+    clear
+    echo "${YELLOW}rclone beta installed"
+    echo "${VERSION}"
+    echo
 }
 
 universals() {
     echo
-    echo "Please enter service account file path, for example /opt/sa/mounts :"
+    echo "${YELLOW}Please enter service account file path, for example /opt/sa/mounts :${RESET}"
     read -r SAPATH
     echo
-    echo "Please enter name to use for rclone union mount, eg reunion."
+    echo "${YELLOW}Please enter name to use for rclone union mount, eg reunion :${RESET}"
     read -r RCUNAME
     MNTPNT="/mnt/${RCUNAME}"
     echo
-    echo "You currently have ${FREESPACE},"
-    echo "DO NOT use all of this for cache."
-    echo Suggest using no more than "${RECSPACE}"G
+    echo "${BYELLOW}You currently have ${FREESPACE},"
+    echo "${BYELLOW}DO NOT use all of this for cache."
+    echo "${BYELLOW}""${BOLD}"Suggest using no more than "${RED}""${RECSPACE}"G "${RESET}"
     echo
-    echo "Please enter cache max size +G (ex 50G)"
+    echo ${YELLOW}"Please enter cache max size +G (ex ${RESET}50G${YELLOW})"${RESET}
     read -r VFSCACHESIZE
     echo
 }
 
 driveadd() {
     echo
-    echo "Please enter a share drive name :"
+    echo "${YELLOW}Please enter a share drive name :${RESET}"
     read -r SDNAME
     echo
-    echo "Please enter ""${SDNAME}"" Drive ID, for example 0A1xxxxxxxxxUk9PVA :"
+    echo "${YELLOW}Please enter ""${SDNAME}"" Drive ID, for example ${RESET}0A1xxxxxxxxxUk9PVA ${YELLOW}:${RESET}"
     read -r "SDID"
     SAFILE="$(shuf -n1 -e "${SAPATH}"/*.json)"
     driveconfig
@@ -134,19 +182,19 @@ driveadd() {
 }
 
 arewedone() {
-    read -r -p "Would you like to add another drive?  [Y/N] : " i
+    read -r -p "${YELLOW}Would you like to add another drive?  [Y/N] : ${RESET}" i
     case $i in
     [yY])
-        echo -e "adding more drives"
+        echo -e "${YELLOW}adding more drives"
         echo
         driveadd
         ;;
     [nN])
-        echo -e "lettuce build mounts"
+        echo -e "${YELLOW}lettuce build mounts"
         return 0
         ;;
     *)
-        echo "Invalid Option"
+        echo "${BRED}Invalid Option"
         arewedone
         ;;
     esac
@@ -166,21 +214,23 @@ fusebox() {
 
 sdrmconfig() {
     echo
-    echo creating rclone config for "${SDNAME}" with ID "${SDID}"
+    echo "${YELLOW}"creating rclone config for "${SDNAME}" with ID "${SDID}" "${RESET}"
+    echo
     rclone config create "${SDNAME}" drive scope drive server_side_across_configs true team_drive "$SDID" service_account_file "${SAFILE}"
     echo
 }
 
 rcunionconfig() {
-    echo creating "${RCUNAME}" rclone union config with default policy options.
+    echo "${YELLOW}"creating "${RCUNAME}" rclone union config with default policy options.
     echo "ACTION:  epall"
     echo "CREATE:  epmfs"
     echo "SEARCH:  ff"
-    echo
+    echo "${RESET}"
     rclone config create "${RCUNAME}" union upstreams "${STRUNION}"
 }
 
 carryon() {
+    echo "${YELLOW}Continue with installation?${RESET}"
     select yn in "Continue" "Exit"; do
         case $yn in
         Continue) break ;;
@@ -196,12 +246,12 @@ mkmounce() {
 }
 
 checkport() {
-    echo "checking if port ${RCLONE_RC_PORT} is already in use"
+    echo "${GREEN}checking if port ${RCLONE_RC_PORT} is already in use"
     if [[ $(sudo lsof -i:"${RCLONE_RC_PORT}") != *localhost* ]]; then
-        echo "port ${RCLONE_RC_PORT} is available and will be used"
+        echo "${YELLOW}port ${RCLONE_RC_PORT} is available and will be used"
     else
         RCLONE_RC_PORT=$((RCLONE_RC_PORT + 1))
-        echo "setting port to ${RCLONE_RC_PORT}"
+        echo "${BGREEN}setting port to ${RCLONE_RC_PORT}"
         checkport
     fi
 }
@@ -267,30 +317,42 @@ enabler() {
 
 cloudboxmsg() {
     if [ $CBI = true ]; then
-        echo "--------------------"
+        echo "${GREEN}--------------------"
         echo "in cloudbox installations the union can be included in mergerfs directory"
         echo "/etc/systemd/system/mergerfs.service can be edited to include ${RCUNAME} eg:-"
-        echo "  /mnt/local=RW:/mnt/remote=NC:/mnt/${RCUNAME}:NC /mnt/unionfs"
-        echo "--------------------"
+        echo "${RESET}  /mnt/local=RW:/mnt/remote=NC:/mnt/${RCUNAME}:NC /mnt/unionfs"
+        echo "${GREEN}--------------------"
         echo
     else
         echo
     fi
 }
 
+exiting() {
+
+    echo "${YELLOW}"
+    echo "    **************************"
+    echo "    * ---------------------- *"
+    echo "    * - install Completed! - *"
+    echo "    * ---------------------- *"
+    echo "    **************************"
+    echo "${RESET}"
+    echo
+}
+
 #________ SET LIST
 
-rooter
+clear
 echo
-rcloneinstall
+rooter
+betachek
 universals
-echo "--------------------"
-echo "User is:          ${USER}"
-echo "SA Path:          ${SAPATH}"
-echo "Cache size:       ${VFSCACHESIZE}"
-echo "Mount Name:       ${RCUNAME}"
-echo "Mount Point:      ${MNTPNT}"
-echo "Continue with installation?"
+echo "${YELLOW}--------------------"
+echo "${RESET}User is:          ${YELLOW}${USER}"
+echo "${RESET}SA Path:          ${YELLOW}${SAPATH}"
+echo "${RESET}Cache size:       ${YELLOW}${VFSCACHESIZE}"
+echo "${RESET}Mount Name:       ${YELLOW}${RCUNAME}"
+echo "${RESET}Mount Point:      ${YELLOW}${MNTPNT}"
 echo "--------------------"
 carryon
 mkmounce
@@ -298,30 +360,32 @@ echo
 rclonecheck
 echo
 fusebox
-echo "add your first drive"
+echo "${YELLOW}add your first drive"
 driveadd
 echo
-echo "Drives added:   ${DRVLIST}"
+echo "${RESET}Drives added:   ${YELLOW}${DRVLIST}"
 echo
 rcunionconfig
 sleep 1
+rclonecheckmate
 echo
 cloudboxer
 echo
 checkport
 echo
-echo "Preparing smount service"
+echo "${YELLOW}Preparing smount service"
 sysdmaker
 enabler
 sudo systemctl daemon-reload
 echo
-echo "starting the ${RCUNAME} service, be patient. If you have a big one this might take a while."
+echo "${YELLOW}starting the ${RCUNAME} service, be patient. If you have a big one this might take a while."
 sudo systemctl start smount.service
 # nohup sh sudo systemctl start smount.service &>/dev/null &
 echo
 cloudboxmsg
-echo "${RCUNAME} smount smounty smounted"
+exiting
+echo "  ${BOLD}${YELLOW}${RCUNAME} smount smounty smounted"
 echo
-echo 'ᕙ(⇀‸↼‶)ᕗ'
+echo '           ᕙ(⇀‸↼‶)ᕗ'
 echo
-echo "please consider reporting any issues"
+echo "  ${RESET}please consider reporting any issues"
